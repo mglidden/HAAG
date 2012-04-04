ba821e1166aa0281061ecf27ed9be5d493c9a3e4
Mason Glidden <mglidden@mit.edu>
2012-04-04 14:00:30 -0400

    basic rails structure

---
+++ haag/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Haag::Application.config.secret_token = '4fb4fb3d2451423c2588025ca70dcc1af9d3de8afb5b9aa79c24169b875ccfe02634c6d82f89cbfc2ebc7b9dc125bcca1004630c2486e855116ed300aeb1db91'
---
+++ haag/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Haag::Application.config.session_store :cookie_store, key: '_haag_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Haag::Application.config.session_store :active_record_store
