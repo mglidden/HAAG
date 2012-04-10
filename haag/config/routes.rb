51d93781bb2bbab286c5e5a16a7adfb45b281bfd
Ricardo Jasinski <jasinski@solvis.com.br>
2012-04-10 08:12:08 -0300

    add model User using Devise

--- haag/config/routes.rb
+++ haag/config/routes.rb
@@ -1,6 +1,10 @@
 Haag::Application.routes.draw do
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
+  resources :users
+  root :to => "home#index"
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
