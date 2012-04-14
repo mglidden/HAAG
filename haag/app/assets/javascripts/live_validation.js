9d46f19281f0029f3df21e68ade17ddf6153e7e4
Ricardo Jasinski <jasinski@vaio-ubuntu.(none)>
2012-04-14 19:13:13 -0400

    move live validation to a separate javascript file

---
+++ haag/app/assets/javascripts/live_validation.js
@@ -0,0 +1,55 @@
+// http://www.emersonlackey.com/article/rails-ajax-validations-jquery
+
+function validate_field(name, value, field) {
+  var postData = { };
+  postData['field'] = name;
+  postData['value'] = value;
+
+  var postRequestCallback =  function(data) {
+    // if array has any contents it should be an error message
+    var dataValid = (data[name].length == 0); 
+
+    if (dataValid) {
+        $(field).parent('p').removeClass('fieldWithErrors');
+        $(field).removeClass('haag-field-with-errors');
+        $(field).next('span').fadeOut(200, function() { $(this).remove(); });
+    } else {
+        $(field).parent('p').addClass('fieldWithErrors');
+        $(field).addClass('haag-field-with-errors');
+        $(field).next('span').remove();
+        $(field).after('<span class="inlineError">' + data[name] + '</span>');
+    }
+  };
+
+  $.post('/courses/validate', postData, postRequestCallback, 'json');
+}
+
+// General delay function, used to watch keyups,
+// Taken from: http://stackoverflow.com/questions/1909441/jquery-keyup-delay
+var delay = (function() {
+  var timer = 0;
+  return function(callback, ms) {
+    clearTimeout(timer);
+    timer = setTimeout(callback, ms);
+  };
+})();
+
+// Checks an array for a passed value
+Array.prototype.in_array = function(p_val) {
+  for(var i = 0, l = this.length; i < l; i++) {
+    if(this[i] == p_val) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// Allows for AJAX
+// Taken from http://henrik.nyh.se/2008/05/rails-authenticity-token-with-jquery
+$(document).ajaxSend(function(event, request, settings) {
+  if( typeof (AUTH_TOKEN) == "undefined")
+    return;
+  // settings.data is a serialized string like "foo=bar&baz=boink" (or null)
+  settings.data = settings.data || "";
+  settings.data += (settings.data ? "&" : "") + "authenticity_token=" + encodeURIComponent(AUTH_TOKEN);
+});
