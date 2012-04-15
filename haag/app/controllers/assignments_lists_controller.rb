6e24068d3a442a05bb579b9be00df434ebb9d7ce
jasinski <jasinski@mit.edu>
2012-04-15 16:41:30 -0400

    proper validation for multiple assignments

--- haag/app/controllers/assignments_lists_controller.rb
+++ haag/app/controllers/assignments_lists_controller.rb
@@ -3,8 +3,8 @@ class AssignmentsListsController < ApplicationController
   
   def new
     @list = AssignmentsList.new
-    assignment = Assignment.new(:creator => current_user)
-    @list.assignments << assignment
+    # assignment = Assignment.new(:creator => current_user)
+    @list.assignments << Assignment.new
     @courses = current_user.courses
   end
   
@@ -13,10 +13,11 @@ class AssignmentsListsController < ApplicationController
     @list = AssignmentsList.new(params[:assignments_list])
     @courses = current_user.courses
 
-      @list.assignments.each { |assignment| assignment.creator = current_user }
+    # 
+    @list.assignments.each { |assignment| assignment.creator = current_user }
 
    
-    if @list.save!
+    if @list.save
       p @list
       @list.assignments.each { |assignment| assignment.save }
       redirect_to root_path, :notice => 'Awesome!'
