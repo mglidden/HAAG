3ea924828940548817e2cfa83620d1f7150bac00
Mason Glidden <mglidden@mit.edu>
2012-05-09 20:18:47 -0400

    added backend ability to switch the currently displayed timeframe

--- haag/app/controllers/dashboard_controller.rb
+++ haag/app/controllers/dashboard_controller.rb
@@ -8,9 +8,12 @@ class DashboardController < ApplicationController
     @user = current_user
     @courses = Course.all
 
-    today = Date.new(Time.new.year, Time.new.month, Time.new.day)
-    @start_day = (today - (today.wday + 7)).mday
-    @days_month = (Date.new(today.year, 12, 31) << (12-today.month)).day
+    delta = params[:delta] ? params[:delta].to_i * 28 : 0
+
+    today = Date.new(Time.new.year, Time.new.month, Time.new.day) + delta
+    @start = (today - (today.wday + 7))
+    @end = @start + 27
+    @days_month = (Date.new(@start.year, 12, 31) << (12-@start.month)).day
 
     @shown_tasks = []
     (0..27).each do |i|
@@ -23,11 +26,11 @@ class DashboardController < ApplicationController
       unless due_date
         break
       end
-      if due_date.year == today.year 
-        if due_date.day >= @start_day and due_date.month == today.month
-          @shown_tasks[due_date.day - @start_day + 1].push(task)
-        elsif due_date.day <= @days_month - 28 + @start_day and 
-          @shown_tasks[due_date.day + @days_month - @start_day + 1].push(task)
+      if due_date >= @start and due_date <= @end
+        if due_date.month == @start.month
+          @shown_tasks[due_date.day - @start.day + 1].push(task)
+        else
+          @shown_tasks[due_date.day + @days_month - @start.day + 1].push(task)
         end
       end
     end
@@ -35,8 +38,8 @@ class DashboardController < ApplicationController
     @mobile_tasks = @shown_tasks
 
     @shown_tasks = @shown_tasks.enum_for(:each_with_index).collect do |tasks, index|
-      day = (index + @start_day - 1) % @days_month + 1
-      month = (index + @start_day - 1) / @days_month + today.month
+      day = (index + @start.mday - 1) % @days_month + 1
+      month = (index + @start.mday - 1) / @days_month + today.month - 1
       year = today.year
       CalDate.new(day, month, year, tasks)
     end    
