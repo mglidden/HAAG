696c9acd5a4449bb7db30b0dd917db28e4210173
jasinski <jasinski@mit.edu>
2012-05-14 00:33:24 -0400

    code cleanup

--- haag/app/controllers/dashboard_controller.rb
+++ haag/app/controllers/dashboard_controller.rb
@@ -21,46 +21,28 @@ class DashboardController < ApplicationController
     end
 
     @user.tasks.each do |task|
-      p "processing task #{task.id}..."
       assignment = task.assignment
       due_date = assignment.due_at
-      p "due date: #{due_date}"
       unless due_date
-        p "no due date!"
         break
       end
-      p "@start: #{@start}, @end: #{@end}, due_date: #{due_date}"
-      p "due_date >= @start --> #{due_date >= @start}"
-      p "due_date <= @end ----> #{due_date <= @end}"
-      #debugger
       if due_date.to_date >= @start and due_date.to_date <= @end
-        p "i'm in!!!"
         if due_date.month == @start.month
           @shown_tasks[due_date.day - @start.day + 1].push(task)
-          p "adding task #{task.id} at position #{due_date.day - @start.day + 1}"
         else
           @shown_tasks[due_date.day + @days_month - @start.day + 1].push(task)
-          p "adding task #{task.id} at position #{due_date.day + @days_month - @start.day + 1}"
         end
       end
     end
     
     @mobile_tasks = @shown_tasks
     
-    p '===================================================='
-    p @user.tasks.count
-    p @shown_tasks.count
-
     @shown_tasks = @shown_tasks.enum_for(:each_with_index).collect do |tasks, index|
-      p "#{index} #{tasks.count}"
       day = (index + @start.mday - 1) % @days_month + 1
       month = (index + @start.mday - 1) / @days_month + today.month - 1
       year = today.year
       CalDate.new(day, month, year, tasks)
     end
-    
-    p @shown_tasks.count
-    p @shown_tasks.collect { |item| item.tasks }.flatten.count
     
     respond_to do |format|
       format.html
